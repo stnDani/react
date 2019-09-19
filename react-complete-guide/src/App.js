f718be5778e4925946f4a90710d2f881af0790b2
stnDani <daniel.florentin.stoian@gmail.com>
2019-09-19 15:43:46 +0300

    CSS to Person component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -29,17 +29,27 @@ class App extends Component {
       persons: [
         { name: 'Max', age: 28 },
         { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
+        { name: 'Stephanie', age: 27 }
       ]
     } )
   }
 
   render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
