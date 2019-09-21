b020ef6e6d5c6d840df1e3e7052ff85551bf7630
stnDani <daniel.florentin.stoian@gmail.com>
2019-09-21 15:53:53 +0300

    component lifecycle

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Person from './Person/Person';
 
-const persons = (props) =>
-    props.persons.map((person, index) => {
+const persons = (props) => {
+    console.log('[Persons.js] rendering...');
+    return props.persons.map((person, index) => {
         return <Person
             key={person.id}
             click={() => props.clicked(index)}
@@ -10,5 +11,6 @@ const persons = (props) =>
             age={person.age}
             changed={(event) => props.changed(event, person.id)} />
     });
+}
 
 export default persons;
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -4,6 +4,12 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: 'asdasd', name: 'Max', age: 28 },
@@ -12,6 +18,15 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] components did mount!');
   }
 
   deletePersonHandler = (personIndex) => {
@@ -46,6 +61,8 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render');
+
     let persons = null;
 
     if (this.state.showPersons) {
@@ -60,6 +77,7 @@ class App extends Component {
     return (
       <div className={styles.App}>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons} 
           clicked={this.togglePersonsHandler}/>
