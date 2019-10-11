f593348161e6497476890da9fbf8cb06e209ddc6
stnDani <daniel.florentin.stoian@gmail.com>
2019-10-11 15:18:55 +0300

    Higher Order Component

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
 
@@ -85,7 +86,7 @@ class App extends Component {
     }
 
     return (
-      <div className={styles.App}>
+      <WithClass classes={styles.App}>
         <button onClick={() => {
           this.setState({ showCockpit: false });
         }}>
@@ -99,7 +100,7 @@ class App extends Component {
             clicked={this.togglePersonsHandler}
           /> : null}
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
