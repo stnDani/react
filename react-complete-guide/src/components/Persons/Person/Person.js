5a1197723e3da9cbc423cc10688310c7822e6ada
stnDani <daniel.florentin.stoian@gmail.com>
2019-10-11 15:12:25 +0300

    rendering adjacent jsx - hoc

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import './Person.css';
+import Aux from '../../../hoc/Auxiliary';
+import classes from './Person.css';
 
+//instead of aux we can use the Fragment component
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className="Person">
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="type" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            <Aux>
+                <p key="i1" onClick={this.props.click}>
+                    I'm {this.props.name} and I am {this.props.age} years old!
+                </p>,
+                <p key="i2">{this.props.children}</p>,
+                <input key="i3" type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                />
+            </Aux>
         );
     }
 };
